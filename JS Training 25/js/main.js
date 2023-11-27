659b2cef061782232b7b9aa9dac0338964b0c594
Ale V <alejandro.the.villate@gmail.com>
2023-11-27 15:41:53 -0500

    completed lesson 25

---
+++ JS Training 25/js/main.js
@@ -0,0 +1,14 @@
+// Higher Order Functions
+
+
+// A higher order function is a function that does at least one of the following:
+// ** Takes one or more functions as an argument (parameter)
+// ** Returns a function as its result
+
+//forEach()
+
+//filter()
+
+//map()
+
+//reduce()
